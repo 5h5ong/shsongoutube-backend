a66dd8de8bb8057fbfe1c2db7ab8023ebfdbc762
5h5ong <Call5h5ong@gmail.com>
2019-10-01 13:01:08 +0900

    move all passport things

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,11 @@
 import { GraphQLServer } from 'graphql-yoga';
-import { createConnection, getRepository } from 'typeorm';
-import { ExtractJwt, Strategy } from 'passport-jwt';
-import * as passport from 'passport';
+import { createConnection } from 'typeorm';
 import schema from './schema';
-import { User } from './entities/User';
 import { passportMiddleware } from './passport';
 
 const server = new GraphQLServer({ schema });
 
-const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'fsihsajhk238913r818f4u'
-};
-
-const verifyUser = async (payload, done) => {
-  const user = await getRepository(User).findOne(payload.id);
-  if (user) {
-    return done(null, user);
-  } else return done(null, false);
-};
-
 server.use(passportMiddleware);
-
-passport.use(new Strategy(jwtOptions, verifyUser));
 
 createConnection()
   .then(() => {
--- src/passport.ts
+++ src/passport.ts
@@ -1,4 +1,21 @@
 import * as passport from 'passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { getRepository } from 'typeorm';
+import { User } from './entities/User';
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'fsihsajhk238913r818f4u'
+};
+
+const verifyUser = async (payload, done) => {
+  const user = await getRepository(User).findOne(payload.id);
+  if (user) {
+    return done(null, user);
+  } else return done(null, false);
+};
+
+passport.use(new Strategy(jwtOptions, verifyUser));
 
 export const passportMiddleware = (req, res, next) => {
   passport.authenticate('jwt', { sessions: false }, (error, user) => {
