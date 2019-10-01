15dab886876f915673289125c05cae099266d782
5h5ong <Call5h5ong@gmail.com>
2019-10-01 21:39:55 +0900

    var name change

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { GraphQLServer } from 'graphql-yoga';
 import { createConnection } from 'typeorm';
 import schema from './schema';
 import { passportMiddleware } from './passport';
-import { getVideo } from './libs/VideoLibs';
+import { getVideoPath } from './libs/VideoLibs';
 
 const server = new GraphQLServer({ schema });
 
 server.use(passportMiddleware);
 server.post('/video', (req, res) => {
   const videoName = 'sample-video.mp4';
-  const video = getVideo(videoName);
+  const video = getVideoPath(videoName);
   res.sendFile(video, err => {
     if (err) {
       throw new Error('영상 전달에 실패하였습니다.');
--- src/libs/VideoLibs.ts
+++ src/libs/VideoLibs.ts
@@ -1,11 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-export const getVideo = (videoName: string) => {
+export const getVideoPath = (videoName: string) => {
   const videoLocation = path.join(__dirname, '/../../sample-video', videoName);
   return videoLocation;
-  const videoBuffer = fs.readFileSync(videoLocation);
-  // change arraybuffer -> For use in Blob
-  const videoArraybuffer = Uint8Array.from(videoBuffer).buffer;
-  return videoArraybuffer;
 };
