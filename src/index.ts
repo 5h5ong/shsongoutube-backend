f1c268d770c918a74584dbfae94183f8455fad59
5h5ong <Call5h5ong@gmail.com>
2019-10-06 16:55:15 +0900

    Delete express get related to streaming

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,6 @@ const server = new GraphQLServer({ schema });
 
 server.use(passportMiddleware);
 
-server.get('/video/:filename', (req, res, next) => {
-  const { filename } = req.params;
-  const videoPath: string = getVideoPath(filename);
-  if (fs.existsSync(videoPath)) {
-    const videoStream = fs.createReadStream(videoPath);
-    videoStream.pipe(res);
-  } else {
-    res.status(404).send('파일 없다 ㅡㅡ');
-  }
-});
-
 createConnection()
   .then(() => {
     server.start(() => console.log('Server is running on localhost:4000'));
