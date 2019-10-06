e369d5bbb1e37698098cb5635a8df646bfd0ee20
5h5ong <Call5h5ong@gmail.com>
2019-10-06 17:14:46 +0900

    Check user exists

--- src/graphql/User/getUser/getUser.ts
+++ src/graphql/User/getUser/getUser.ts
@@ -3,8 +3,13 @@ import { User } from '../../../entities/User';
 
 export default {
   Query: {
-    getUser: (_, { id }) => {
-      return getRepository(User).findOne(id);
+    getUser: async (_, { id }) => {
+      const user = await getRepository(User).findOne(id);
+      if (user) {
+        return user;
+      } else if (!user) {
+        throw Error('해당 유저는 존재하지 않습니다.');
+      }
     }
   }
 };
