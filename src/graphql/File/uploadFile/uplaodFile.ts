439893a7c27b76eaab725f3e520921702df5f693
5h5ong <Call5h5ong@gmail.com>
2019-10-15 23:48:03 +0900

    make can add a preview image

--- src/graphql/File/uploadFile/uplaodFile.ts
+++ src/graphql/File/uploadFile/uplaodFile.ts
@@ -3,11 +3,12 @@ import { getRepository } from 'typeorm';
 
 export default {
   Mutation: {
-    uploadFile: (_, { url, filename }) => {
+    uploadFile: (_, { url, filename, preview }) => {
       const file = new File();
       // set File Entity
       file.filename = filename;
       file.url = url;
+      file.preview = preview;
       // File 반영
       const result = getRepository(File).save(file);
       if (!result) {
