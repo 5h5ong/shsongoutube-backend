6aaab45580882f33225d96dfe6a1a747cc36e742
5h5ong <Call5h5ong@gmail.com>
2019-10-20 19:29:16 +0900

    isLiked 구현

---
+++ src/graphql/File/File.ts
@@ -0,0 +1,18 @@
+import { getRepository } from 'typeorm';
+import { User } from '../../entities/User';
+import { File } from '../../entities/File';
+export default {
+  File: {
+    isLiked: async (parent, __, { request }) => {
+      const { user } = request;
+      // user의 id로 User 안에 있는 files와 비교해서 일치 여부에 따라 true / false
+      try {
+        const foundUser = await getRepository(User).findOne({ id: user.id });
+        const files = await foundUser.files;
+        return Boolean(files.find(file => file.id === parent.id));
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+};
